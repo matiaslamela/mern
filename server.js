61213c5c1e06d917e5aa2cf05f0a7173a7d2cad9
Matias Lamela <matiaslamela1992@gmail.com>
2020-08-28 20:51:56 -0300

    cambie el store

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
 	.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => console.log('mongoDB conectada'))
 	.catch((err) => console.log(err));
-
+//process.env
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('../client/build'));
 	app.get('*', (req, res) => {
